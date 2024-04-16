a3f5a9611f0b4ee767a09c9ce848d4e1f41ab23e
asanzo <alfredo.sanzo@gmail.com>
2024-04-15 22:11:08 -0300

    Subo código hecho en clase

--- src/Library.hs
+++ src/Library.hs
@@ -2,9 +2,73 @@ module Library where
 import PdePreludat
 
 -- Punto 1
--- (agregar el tipo)
-esMultiploDe = implementame
+esMultiploDe :: Number -> Number -> Bool
+esMultiploDe dividendo divisor = rem dividendo divisor == 0
 
 -- Punto 2
--- (agregar el tipo)
-esBisiesto = implementame
+esBisiesto :: Number -> Bool
+esBisiesto anio = 
+    esMultiploDe anio 400 || 
+    esMultiploDe anio 4 && not (esMultiploDe anio 100)
+
+
+-- Punto 3
+cantidadDeDias :: Number -> Number
+cantidadDeDias anio | esBisiesto anio = 366
+    | otherwise = 365
+
+
+anioActual :: Number
+anioActual = 2024
+
+-- Una persona tiene nombre y anio de nacimiento.
+data Persona = UnaPersona {
+    nombreDePila :: String,
+    anioNac :: Number,
+    mascotas :: [Mascota]
+} deriving (Show)
+
+-- edad, que dada una persona me diga su edad.
+edad :: Persona -> Number
+edad alguien = anioActual - anioNac alguien
+
+
+-- datos de prueba
+martin :: Persona
+martin = UnaPersona { 
+    nombreDePila = "El Marto", 
+    anioNac = 1983,
+    mascotas = [felipe, nina]
+}
+
+-- Saber la inicial de una persona
+inicial :: Persona -> Char
+inicial = head . nombreDePila
+--inicial alguien = head (nombreDePila alguien)
+
+-- Las personas tienen mascotas, de las cuales sabemos su nombre, raza y si están vacunadas.
+
+-- ¿Cómo modelo esto?
+
+data Mascota = UnaMascota {
+    nombre :: String,
+    raza :: String,
+    estaVacunada :: Bool
+} deriving (Show)
+
+-- datos de ejemplo
+felipe :: Mascota
+felipe = UnaMascota { nombre = "Feli", raza = "labrador", estaVacunada = True }
+
+nina :: Mascota
+nina = UnaMascota { nombre = "Nina", raza = "golden", estaVacunada = False }
+
+-- Variables de tipo
+miConst :: a -> b -> a
+miConst x y = x
+
+-- una persona es feliz cuando tiene al menos una mascota
+
+esFeliz :: Persona -> Bool
+esFeliz = not . null . mascotas
+-- esFeliz alguien = length (mascotas alguien) > 0
